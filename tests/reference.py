e75f1db920ccf0ef8ae47284b1763f127334be8c
Igor Krivenko <iskrivenko@proton.me>
2025-06-17 14:17:10 +0200

    Generate reference data for solver tests using pomerol2triqs instead of AtomDiag
    * Validate computed values of the superconducting order parameter \phi.
    * Tweak parameters of test Hamiltonians so that more involved physical regimes
      are explored.

--- tests/reference.py
+++ tests/reference.py
@@ -1,16 +1,11 @@
 """
-Generate reference results for TestEDIpackSolver* using AtomDiag.
+Generate reference results for TestEDIpackSolver* using pomerol2triqs.
 """
 
 import inspect
 import numpy as np
+from itertools import product
 
-import triqs.operators as op
-from triqs.atom_diag import (AtomDiag,
-                             atomic_density_matrix,
-                             atomic_g_iw,
-                             atomic_g_w,
-                             trace_rho_op)
 from triqs.gf.tools import dyson
 from h5 import HDFArchive
 
@@ -52,54 +47,89 @@ def make_reference_results(*,
                            spin_blocks=True,
                            superc=False,
                            zerotemp=False):
-    "Generate reference results"
+    "Generate reference results using pomerol2triqs"
+
+    from pomerol2triqs import PomerolED
 
     up, dn = spins
     mki = make_mkind(spins, orbs, spin_blocks)
+    norb = len(orbs)
 
-    c_up = [op.c(*mki(up, o)) for o in orbs]
-    c_dn = [op.c(*mki(dn, o)) for o in orbs]
-    c_dag_up = [op.c_dag(*mki(up, o)) for o in orbs]
-    c_dag_dn = [op.c_dag(*mki(dn, o)) for o in orbs]
-    n_up = [op.n(*mki(up, o)) for o in orbs]
-    n_dn = [op.n(*mki(dn, o)) for o in orbs]
+    # Conversion from TRIQS to Pomerol notation for operator indices
+    # TRIQS: block_name, inner_index
+    # Pomerol: site_label, orbital_index, spin_name
+    index_converter = {}
+    for bn, inner in fops:
+        # Bath
+        if bn[0] == "B":  # Bath
+            site = "B"
+            spin = "down" if bn[-2:] == dn else up
+            orb = inner
+        else:  # local
+            site = "L"
+            if spin_blocks:
+                spin = "down" if bn[-2:] == dn else up
+            else:
+                spin = "down" if inner >= norb else up
+            orb = inner % norb
+        index_converter[(bn, inner)] = (site, orb, spin)
 
-    N = [n_up[o] + n_dn[o] for o in orbs]
-    D = [n_up[o] * n_dn[o] for o in orbs]
-    S_x = [c_dag_up[o] * c_dn[o] + c_dag_dn[o] * c_up[o] for o in orbs]
-    S_y = [c_dag_dn[o] * c_up[o] - c_dag_up[o] * c_dn[o] for o in orbs]
-    S_z = [n_up[o] - n_dn[o] for o in orbs]
+    def make_ed(h_):
+        ed = PomerolED(index_converter, verbose=False)
+        ed.ops_melem_tol = 0
+        ed.rho_threshold = 0
+        ed.diagonalize(h_)
+        return ed
 
+    ed = make_ed(h)
+
+    def make_avg(spins):
+        res = [ed.ensemble_average(*[mki(s, o) for s in spins], beta)
+               for o in orbs]
+        return np.array(res)
+
+    # Occupation and magnetization
+    n_up_avg = make_avg((up, up))
+    n_dn_avg = make_avg((dn, dn))
+    S_p_avg = make_avg((up, dn))
+    S_m_avg = make_avg((dn, up))
+    # Double occupancy
+    D_avg = make_avg((up, dn, dn, up))
+
+    # Green's functions
     if spin_blocks:
-        gf_struct = [(up, len(orbs)), (dn, len(orbs))]
+        gf_struct = [(up, norb), (dn, norb)]
     else:
-        gf_struct = [(f'{up}_{dn}', 2 * len(orbs))]
+        gf_struct = [(f"{up}_{dn}", 2 * norb)]
 
-    ad = AtomDiag(h, fops)
-    rho = atomic_density_matrix(ad, beta)
-    g_w = atomic_g_w(ad, beta, gf_struct, energy_window, n_w, broadening)
+    tols = {"pole_res": 1e-12, "coeff_tol": 1e-12}
+    g_w = ed.G_w(gf_struct, beta, energy_window, n_w, broadening, **tols)
 
-    def avg(ops):
-        return np.array([trace_rho_op(rho, ops[o], ad) for o in orbs])
-
-    results = {'densities': avg(N),
-               'double_occ': avg(D),
-               'magn_x': avg(S_x),
-               'magn_y': 1j * avg(S_y),
-               'magn_z': avg(S_z),
+    results = {'densities': n_up_avg + n_dn_avg,
+               'double_occ': D_avg,
+               'magn_x': S_p_avg + S_m_avg,
+               'magn_y': -1j * (S_p_avg - S_m_avg),
+               'magn_z': n_up_avg - n_dn_avg,
                'g_w': g_w}
 
+    # Superconductive \phi
+    if superc:
+        phi = np.empty((norb, norb), dtype=complex)
+        for o1, o2 in product(orbs, repeat=2):
+            phi[o1, o2] = ed.ensemble_average(mki(up, o1), mki(dn, o2),
+                                              beta, (False, False))
+        results['phi'] = phi
+
     if not zerotemp:
-        g_iw = atomic_g_iw(ad, beta, gf_struct, n_iw)
+        g_iw = ed.G_iw(gf_struct, beta, n_iw, **tols)
         results['g_iw'] = g_iw
 
     if not superc:
-        h0 = non_int_part(h)
-        ad0 = AtomDiag(h0, fops)
-        g0_w = atomic_g_w(ad0, beta, gf_struct, energy_window, n_w, broadening)
+        ed0 = make_ed(non_int_part(h))
+        g0_w = ed0.G_w(gf_struct, beta, energy_window, n_w, broadening, **tols)
         results['Sigma_w'] = dyson(G0_iw=g0_w, G_iw=g_w)
         if not zerotemp:
-            g0_iw = atomic_g_iw(ad0, beta, gf_struct, n_iw)
+            g0_iw = ed0.G_iw(gf_struct, beta, n_iw, **tols)
             results['Sigma_iw'] = dyson(G0_iw=g0_iw, G_iw=g_iw)
 
     return results
