24ba318e1cae616387ee99b8e1ab5f05e272e08b
Igor Krivenko <iskrivenko@proton.me>
2025-06-24 15:23:08 +0200

    Extend unit testing to anomalous Green's functions and self-energies

--- tests/test_solver_general.py
+++ tests/test_solver_general.py
@@ -133,13 +133,10 @@ class TestEDIpackSolverBathGeneral(unittest.TestCase):
                 refs['phi'],
                 atol=1e-8
             )
-        assert_block_gfs_are_close(s.g_w, refs['g_w'])
-        if 'g_iw' in refs:
-            assert_block_gfs_are_close(s.g_iw, refs['g_iw'])
-        if 'Sigma_w' in refs:
-            assert_block_gfs_are_close(s.Sigma_w, refs['Sigma_w'])
-        if 'Sigma_iw' in refs:
-            assert_block_gfs_are_close(s.Sigma_iw, refs['Sigma_iw'])
+        for gf in ('g_iw', 'g_an_iw', 'Sigma_iw', 'Sigma_an_iw',
+                   'g_w', 'g_an_w', 'Sigma_w', 'Sigma_an_w'):
+            if gf in refs:
+                assert_block_gfs_are_close(getattr(s, gf), refs[gf])
 
     @classmethod
     def find_basis_mat(cls, hvec, mat):
@@ -668,7 +665,6 @@ class TestEDIpackSolverBathGeneral(unittest.TestCase):
         V = np.array([[0.2, -0.2],
                       [0.4, -0.4]])
         h_bath = self.make_h_bath(mul.outer(s0, h), mul.outer([1, 1], V))
-
         Delta = np.moveaxis(np.array([[[0.3, 0.2j],
                                        [0.2j, 0.4]],
                                       [[0.3, 0.0],
@@ -776,12 +772,6 @@ class TestEDIpackSolverBathGeneral(unittest.TestCase):
         refs = ref.ref_results("superc_3", h=h, superc=True,
                                **struct_params, **solve_params)
         self.assert_all(solver, **refs)
-
-        # TODO
-        solver.g_an_iw
-        solver.Sigma_an_iw
-        solver.g_an_w
-        solver.Sigma_an_w
 
     def tearDown(self):
         # Make sure EDIpackSolver.__del__() is called
