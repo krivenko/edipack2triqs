0e244c2d13b489a75ca528d96ff6701108fe0912
Igor Krivenko <iskrivenko@proton.me>
2026-01-22 19:29:32 +0100

    test: Correction to 22269047d2d7a0d32a5517b11a7fdc1e2b4cf57c

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -183,10 +183,10 @@ class TestSolver(unittest.TestCase):
     @classmethod
     def change_int_params(cls, U, *, Uloc, Ust, Jh, Jx, Jp):
         # Uloc
-        for s, o in product(cls.orbs, cls.orbs):
+        for s, o in product(range(2), cls.orbs):
             U[o, s, o, 1 - s, o, s, o, 1 - s] = 0.5 * Uloc[o]
             U[o, s, o, 1 - s, o, 1 - s, o, s] = -0.5 * Uloc[o]
-        for s, o1, o2 in product(cls.orbs, cls.orbs, cls.orbs):
+        for s, o1, o2 in product(range(2), cls.orbs, cls.orbs):
             if o1 == o2:
                 continue
             # Ust
